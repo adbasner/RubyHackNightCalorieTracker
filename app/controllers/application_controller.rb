8a959b2958bc5e7ffd09918e83bb0f735ef59b34
Andrew Basner <andrewbasner@gmail.com>
2020-01-08 20:36:19 -0600

    make rest call to nutri and display basic info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def content_not_found
+    render 'pages#main'
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def main
+    search_param = params[:search]
+    @declaration = ""
+    if search_param != ""
+      api_key = Rails.application.credentials.dig(:nutritionix_api)
+      headers = {"x-app-id": api_key[:app_id], "x-app-key": api_key[:app_key]}
+      url = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+      body = {"query": search_param }
+
+      response = RestClient.post(url, body, headers)
+      if response != nil
+        @declaration = "You searched for #{search_param}"
+        @response = JSON.parse(response.body)
+      else
+        content_not_found
+      end
+    else 
+      @declaration = "Search for a food item"
+    end
   end
 end
